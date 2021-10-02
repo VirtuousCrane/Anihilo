897460cf8b6d26e111fb218f64c6d54c499ca859
Janthanat Thanintantrakun <beamnut@hotmail.com>
2021-10-02 15:41:27 +0700

    feat: add frame for main menu

--- Frontend/src/Main_GUI.java
+++ Frontend/src/Main_GUI.java
@@ -3,6 +3,7 @@ import main_menu.Main_Menu_Page;
 
 public class Main_GUI {
     public static void main(String[] args) throws Exception {
-        new Main_Game_Page();
+        new Main_Menu_Page();
+        // new Main_Game_Page();
     }
 }
--- Frontend/src/main_menu/Main_Menu_Page.java
+++ Frontend/src/main_menu/Main_Menu_Page.java
@@ -1,5 +1,22 @@
 package main_menu;
 
-public class Main_Menu_Page {
+import javax.swing.JFrame;
 
+public class Main_Menu_Page {
+    public Main_Menu_Page() {
+        JFrame main_page = new JFrame();
+
+        // * Setting Frame Size and etc
+        main_page.setSize(750, 600);
+        main_page.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main_page.setLayout(null);
+
+        // * set Title
+        main_page.setTitle("Higher or Lower");
+
+        // * Instruction
+
+        // * make JFrame visible (need to be put last)
+        main_page.setVisible(true);
+    }
 }
