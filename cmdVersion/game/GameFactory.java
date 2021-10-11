db3b70d69f4cd9686895156f901f23d79741d405
Chatchat Paisalpanich <frinttropystudy222@gmail.com>
2021-10-11 15:29:17 +0700

    Added GameFactory()

---
+++ cmdVersion/game/GameFactory.java
@@ -0,0 +1,34 @@
+package cmdVersion.game;
+
+import cmdVersion.game.lifeControl.LifeControl;
+import cmdVersion.game.lifeControl.StageLifeControl;
+import cmdVersion.game.questionControl.QuestionControl;
+import cmdVersion.game.questionControl.StageQuestionControl;
+import cmdVersion.game.scoreControl.ScoreControl;
+import cmdVersion.game.scoreControl.StageScoreControl;
+
+
+public class GameFactory {
+    public static final String[] gameModes = {"Classic"};
+
+    public Game createGame(String gameModeName){
+
+        QuestionControl questionControl;
+        ScoreControl scoreControl;
+        LifeControl lifeControl;
+
+        if (gameModeName.equalsIgnoreCase(gameModes[0])){ // Classic
+            questionControl = new StageQuestionControl();
+            scoreControl = new StageScoreControl();
+            lifeControl = new StageLifeControl();
+        } else {
+            System.out.println("Error at createGame");
+            System.out.println("Returning classic game mode");
+            questionControl = new StageQuestionControl();
+            scoreControl = new StageScoreControl();
+            lifeControl = new StageLifeControl();
+        }
+
+        return new Game(questionControl, lifeControl, scoreControl);
+    }
+}
