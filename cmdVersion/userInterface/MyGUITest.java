ad3c614f7ab681fc696a4bb2387448afa2875a3d
Chatchat Paisalpanich <frinttropystudy222@gmail.com>
2021-10-20 15:27:55 +0700

    Added some comments

---
+++ cmdVersion/userInterface/MyGUITest.java
@@ -0,0 +1,19 @@
+package cmdVersion.userInterface;
+
+import javax.swing.*;
+
+public class MyGUITest {
+    public static void main(String[] args){
+        JFrame main_page = new JFrame();
+
+        // * Setting Frame Size and etc
+        main_page.setSize(750, 600);
+        main_page.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main_page.setLayout(null);
+        
+
+
+        // * set Title
+        main_page.setTitle("Higher or Lower");
+    }
+}
